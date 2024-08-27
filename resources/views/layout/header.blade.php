aeb009cf1f85a2df691bd67e0e48be8a77d41e03
Nghia <trinhvantrungnghia@gmail.com>
2024-08-27 11:10:47 +0700

    create auth

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -84,7 +84,36 @@
                     <img class="logoFb" loading="lazy" alt="" src="{{ ('/frontEnd/img/logoFb.png') }}">
                     <img class="logoIg" loading="lazy" alt="" src="{{ ('/frontEnd/img/logoIg.png') }}">
                     <img class="logoYoutube" loading="lazy" alt="" src="{{ ('/frontEnd/img/logoYoutube.png') }}">
+                
                 </div>
+                @if (Route::has('login'))
+                            <nav class="-mx-3 flex flex-1 justify-end">
+                                @auth
+                                    <a
+                                        href="{{ url('/admin') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        admin
+                                    </a>
+                                @else
+                                    <a
+                                        href="{{ route('login') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Log in
+                                    </a>
+
+                                    @if (Route::has('register'))
+                                        <a
+                                            href="{{ route('register') }}"
+                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            Register
+                                        </a>
+                                    @endif
+                                @endauth
+                            </nav>
+                        @endif
               </div>
                     
                   
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategoryController;
@@ -19,18 +21,19 @@ use App\Http\Controllers\ImgMainPageController;
 use App\Http\Controllers\KhamPhaController;
 use App\Http\Controllers\AdminManageController;
 use App\Http\Controllers\AreaController;
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// ------------------------MainPage------------------------------------------
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-// Route::get('/', function () {
-//     return view('homePage');
-// });
+require __DIR__.'/auth.php';
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/trangchu', [HomeController::class, 'index']);
@@ -95,89 +98,87 @@ Route::get('/sukien/{eID}', [SuKienController::class, 'sukienchitiet']);
 Route::get('/admin', [AdminManageController::class, 'index']);
 
 // ---------------------------Area--------------------
-Route::get('/area_manage', [AreaController::class, 'area_manage']);
-Route::get('/add_area', [AreaController::class, 'add_area']);
-Route::post('/saveArea', [AreaController::class, 'saveArea']);
-Route::get('/editArea/{areaID}', [AreaController::class, 'editArea']);
-Route::post('/updateArea/{areaID}', [AreaController::class, 'updateArea']);
-Route::get('/deleteArea/{areaID}', [AreaController::class, 'deleteArea']);
+Route::get('/area_manage', [AreaController::class, 'area_manage'])->middleware(['auth', 'verified']);
+Route::get('/add_area', [AreaController::class, 'add_area'])->middleware(['auth', 'verified']);
+Route::post('/saveArea', [AreaController::class, 'saveArea'])->middleware(['auth', 'verified']);
+Route::get('/editArea/{areaID}', [AreaController::class, 'editArea'])->middleware(['auth', 'verified']);
+Route::post('/updateArea/{areaID}', [AreaController::class, 'updateArea'])->middleware(['auth', 'verified']);
+Route::get('/deleteArea/{areaID}', [AreaController::class, 'deleteArea'])->middleware(['auth', 'verified']);
 
 // ------------------------Category---------------------------------------------
-Route::get('/category_manage', [CategoryController::class, 'category_manage']);
-Route::get('/add_category', [CategoryController::class, 'index']);
-Route::post('/saveCategory', [CategoryController::class, 'saveCategory']);
-Route::get('/editCategory/{categoryID}', [CategoryController::class, 'editCategory']);
-Route::post('/updateCategory/{categoryID}', [CategoryController::class, 'updateCategory']);
-Route::get('/deleteCategory/{categoryID}', [CategoryController::class, 'deleteCategory']);
+Route::get('/category_manage', [CategoryController::class, 'category_manage'])->middleware(['auth', 'verified']);
+Route::get('/add_category', [CategoryController::class, 'index'])->middleware(['auth', 'verified']);
+Route::post('/saveCategory', [CategoryController::class, 'saveCategory'])->middleware(['auth', 'verified']);
+Route::get('/editCategory/{categoryID}', [CategoryController::class, 'editCategory'])->middleware(['auth', 'verified']);
+Route::post('/updateCategory/{categoryID}', [CategoryController::class, 'updateCategory'])->middleware(['auth', 'verified']);
+Route::get('/deleteCategory/{categoryID}', [CategoryController::class, 'deleteCategory'])->middleware(['auth', 'verified']);
 
 // ----------------------------Location----------------------------------------
-Route::get('/location_manage', [LocationController::class, 'location_manage']);
-Route::get('/add_location', [LocationController::class, 'index']);
-Route::post('/saveLocation', [LocationController::class, 'saveLocation']);
-Route::get('/editLocation/{loID}', [LocationController::class, 'editLocation']);
-Route::post('/updateLocation/{loID}', [LocationController::class, 'updateLocation']);
-Route::get('/deleteLocation/{loID}', [LocationController::class, 'deleteLocation']);
+Route::get('/location_manage', [LocationController::class, 'location_manage'])->middleware(['auth', 'verified']);
+Route::get('/add_location', [LocationController::class, 'index'])->middleware(['auth', 'verified']);
+Route::post('/saveLocation', [LocationController::class, 'saveLocation'])->middleware(['auth', 'verified']);
+Route::get('/editLocation/{loID}', [LocationController::class, 'editLocation'])->middleware(['auth', 'verified']);
+Route::post('/updateLocation/{loID}', [LocationController::class, 'updateLocation'])->middleware(['auth', 'verified']);
+Route::get('/deleteLocation/{loID}', [LocationController::class, 'deleteLocation'])->middleware(['auth', 'verified']);
 
 // ------------------------------Location Img-------------------------------------
-Route::get('/locationImg_manage', [LocationImgController::class, 'locationImg_manage']);
-Route::get('/add_locationImg', [LocationImgController::class, 'index']);
-Route::post('/saveLocationImg', [LocationImgController::class, 'saveLocationImg']);
-Route::get('/editLocationImg/{loImgID}', [LocationImgController::class, 'editLocationImg']);
-Route::post('/updateLocationImg/{loImgID}', [LocationImgController::class, 'updateLocationImg']);
-Route::get('/deleteLocationImg/{loImgID}', [LocationImgController::class, 'deleteLocationImg']);
+Route::get('/locationImg_manage', [LocationImgController::class, 'locationImg_manage'])->middleware(['auth', 'verified']);
+Route::get('/add_locationImg', [LocationImgController::class, 'index'])->middleware(['auth', 'verified']);
+Route::post('/saveLocationImg', [LocationImgController::class, 'saveLocationImg'])->middleware(['auth', 'verified']);
+Route::get('/editLocationImg/{loImgID}', [LocationImgController::class, 'editLocationImg'])->middleware(['auth', 'verified']);
+Route::post('/updateLocationImg/{loImgID}', [LocationImgController::class, 'updateLocationImg'])->middleware(['auth', 'verified']);
+Route::get('/deleteLocationImg/{loImgID}', [LocationImgController::class, 'deleteLocationImg'])->middleware(['auth', 'verified']);
 
 // ------------------------------ThucVat-------------------------------------
-Route::get('/thucvat_manage', [ThucVatController::class, 'thucvat_manage']);
-Route::get('/add_thucvat', [ThucVatController::class, 'index']);
-Route::post('/saveThucVat', [ThucVatController::class, 'saveThucVat']);
-Route::get('/editThucVat/{tvID}', [ThucVatController::class, 'editThucVat']);
-Route::post('/updateThucVat/{tvID}', [ThucVatController::class, 'updateThucVat']);
-Route::get('/deleteThucVat/{tvID}', [ThucVatController::class, 'deleteThucVat']);
+Route::get('/thucvat_manage', [ThucVatController::class, 'thucvat_manage'])->middleware(['auth', 'verified']);
+Route::get('/add_thucvat', [ThucVatController::class, 'index'])->middleware(['auth', 'verified']);
+Route::post('/saveThucVat', [ThucVatController::class, 'saveThucVat'])->middleware(['auth', 'verified']);
+Route::get('/editThucVat/{tvID}', [ThucVatController::class, 'editThucVat'])->middleware(['auth', 'verified']);
+Route::post('/updateThucVat/{tvID}', [ThucVatController::class, 'updateThucVat'])->middleware(['auth', 'verified']);
+Route::get('/deleteThucVat/{tvID}', [ThucVatController::class, 'deleteThucVat'])->middleware(['auth', 'verified']);
 
 
 // --------------------------imgMainPage----------------------------------------
-Route::get('/imgMainPage_manage', [ImgMainPageController::class, 'imgMainPage_manage']);
-Route::get('/add_imgMainPage', [ImgMainPageController::class, 'index']);
-Route::post('/saveImgMainPage', [ImgMainPageController::class, 'saveImgMainPage']);
-Route::get('/editImgMainPage/{imgID}', [ImgMainPageController::class, 'editImgMainPage']);
-Route::post('/updateImgMainPage/{imgID}', [ImgMainPageController::class, 'updateImgMainPage']);
-Route::get('/deleteImgMainPage/{imgID}', [ImgMainPageController::class, 'deleteImgMainPage']);
+Route::get('/imgMainPage_manage', [ImgMainPageController::class, 'imgMainPage_manage'])->middleware(['auth', 'verified']);
+Route::get('/add_imgMainPage', [ImgMainPageController::class, 'index'])->middleware(['auth', 'verified']);
+Route::post('/saveImgMainPage', [ImgMainPageController::class, 'saveImgMainPage'])->middleware(['auth', 'verified']);
+Route::get('/editImgMainPage/{imgID}', [ImgMainPageController::class, 'editImgMainPage'])->middleware(['auth', 'verified']);
+Route::post('/updateImgMainPage/{imgID}', [ImgMainPageController::class, 'updateImgMainPage'])->middleware(['auth', 'verified']);
+Route::get('/deleteImgMainPage/{imgID}', [ImgMainPageController::class, 'deleteImgMainPage'])->middleware(['auth', 'verified']);
 
 
 // ----------------------------Ticket----------GiaVeController-------------
-Route::get('/ticket_manage', [GiaVeController::class, 'ticket_manage']);
-Route::get('/add_ticket', [GiaVeController::class, 'index']);
-Route::post('/saveTicket', [GiaVeController::class, 'saveTicket']);
-Route::get('/editTicket/{ticketID}', [GiaVeController::class, 'editTicket']);
-Route::post('/updateTicket/{ticketID}', [GiaVeController::class, 'updateTicket']);
-Route::get('/deleteTicket/{ticketID}', [GiaVeController::class, 'deleteTicket']);
+Route::get('/ticket_manage', [GiaVeController::class, 'ticket_manage'])->middleware(['auth', 'verified']);
+Route::get('/add_ticket', [GiaVeController::class, 'index'])->middleware(['auth', 'verified']);
+Route::post('/saveTicket', [GiaVeController::class, 'saveTicket'])->middleware(['auth', 'verified']);
+Route::get('/editTicket/{ticketID}', [GiaVeController::class, 'editTicket'])->middleware(['auth', 'verified']);
+Route::post('/updateTicket/{ticketID}', [GiaVeController::class, 'updateTicket'])->middleware(['auth', 'verified']);
+Route::get('/deleteTicket/{ticketID}', [GiaVeController::class, 'deleteTicket'])->middleware(['auth', 'verified']);
 
 
 
 // -------------------------------TicketImg-------------
-Route::get('/ticketImg_manage', [TicketImgController::class, 'ticketImg_manage']);
-Route::get('/add_ticketImg', [TicketImgController::class, 'index']);
-Route::post('/saveTicketImg', [TicketImgController::class, 'saveTicketImg']);
-Route::get('/editTicketImg/{ticketImgID}', [TicketImgController::class, 'editTicketImg']);
-Route::post('/updateTicketImg/{ticketImgID}', [TicketImgController::class, 'updateTicketImg']);
-Route::get('/deleteTicketImg/{ticketImgID}', [TicketImgController::class, 'deleteTicketImg']);
+Route::get('/ticketImg_manage', [TicketImgController::class, 'ticketImg_manage'])->middleware(['auth', 'verified']);
+Route::get('/add_ticketImg', [TicketImgController::class, 'index'])->middleware(['auth', 'verified']);
+Route::post('/saveTicketImg', [TicketImgController::class, 'saveTicketImg'])->middleware(['auth', 'verified']);
+Route::get('/editTicketImg/{ticketImgID}', [TicketImgController::class, 'editTicketImg'])->middleware(['auth', 'verified']);
+Route::post('/updateTicketImg/{ticketImgID}', [TicketImgController::class, 'updateTicketImg'])->middleware(['auth', 'verified']);
+Route::get('/deleteTicketImg/{ticketImgID}', [TicketImgController::class, 'deleteTicketImg'])->middleware(['auth', 'verified']);
 
 
 // -----------------------------------Event------------------------------
-Route::get('/event_manage', [EventController::class, 'event_manage']);
-Route::get('/add_event', [EventController::class, 'index']);
-Route::post('/saveEvent', [EventController::class, 'saveEvent']);
-Route::get('/editEvent/{eID}', [EventController::class, 'editEvent']);
-Route::post('/updateEvent/{eID}', [EventController::class, 'updateEvent']);
-Route::get('/deleteEvent/{eID}', [EventController::class, 'deleteEvent']);
+Route::get('/event_manage', [EventController::class, 'event_manage'])->middleware(['auth', 'verified']);
+Route::get('/add_event', [EventController::class, 'index'])->middleware(['auth', 'verified']);
+Route::post('/saveEvent', [EventController::class, 'saveEvent'])->middleware(['auth', 'verified']);
+Route::get('/editEvent/{eID}', [EventController::class, 'editEvent'])->middleware(['auth', 'verified']);
+Route::post('/updateEvent/{eID}', [EventController::class, 'updateEvent'])->middleware(['auth', 'verified']);
+Route::get('/deleteEvent/{eID}', [EventController::class, 'deleteEvent'])->middleware(['auth', 'verified']);
 
 
 // -----------------------------------EventImg------------------------------
-Route::get('/eventImg_manage', [EventImgController::class, 'eventImg_manage']);
-Route::get('/add_eventImg', [EventImgController::class, 'index']);
-Route::post('/saveEventImg', [EventImgController::class, 'saveEventImg']);
-Route::get('/editEventImg/{eImgID}', [EventImgController::class, 'editEventImg']);
-Route::post('/updateEventImg/{eImgID}', [EventImgController::class, 'updateEventImg']);
-Route::get('/deleteEventImg/{eImgID}', [EventImgController::class, 'deleteEventImg']);
-
-
+Route::get('/eventImg_manage', [EventImgController::class, 'eventImg_manage'])->middleware(['auth', 'verified']);
+Route::get('/add_eventImg', [EventImgController::class, 'index'])->middleware(['auth', 'verified']);
+Route::post('/saveEventImg', [EventImgController::class, 'saveEventImg'])->middleware(['auth', 'verified']);
+Route::get('/editEventImg/{eImgID}', [EventImgController::class, 'editEventImg'])->middleware(['auth', 'verified']);
+Route::post('/updateEventImg/{eImgID}', [EventImgController::class, 'updateEventImg'])->middleware(['auth', 'verified']);
+Route::get('/deleteEventImg/{eImgID}', [EventImgController::class, 'deleteEventImg'])->middleware(['auth', 'verified']);
